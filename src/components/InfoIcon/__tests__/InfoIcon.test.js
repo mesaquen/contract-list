2c06a99507823da70f73922372cd8da89e5ec717
Mesaque Francisco <mesaquenf@gmail.com>
2021-01-24 17:54:52 -0300

    Adding InfoIcon component

---
+++ src/components/InfoIcon/__tests__/InfoIcon.test.js
@@ -0,0 +1,11 @@
+import { render, cleanup } from '@testing-library/react'
+import InfoIcon from '../index'
+
+beforeEach(cleanup)
+
+describe('InfoIcon', () => {
+  test('Matches snapshot', () => {
+    const { asFragment } = render(<InfoIcon info='Sample info' />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
---
+++ src/components/InfoIcon/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { MdInfo } from 'react-icons/md'
+import './styles.scss'
+
+const InfoIcon = ({ info, size, ...props }) => {
+    
+  return <span title={info}>
+      <MdInfo className='infoIcon--icon' size={size} />
+  </span>
+}
+
+InfoIcon.propTypes = {
+  info: PropTypes.string,
+  size: PropTypes.string,
+}
+
+InfoIcon.defaultProps = {
+  size: '1.5rem',
+}
+
+export default InfoIcon
